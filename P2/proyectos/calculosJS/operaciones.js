48dfae5ce3da2dca41ed1f6c013481ea4b3c2d03
Francisco Javier Vázquez Sandoval <javier.sandoval1123@gmail.com>
2024-10-18 09:45:40 -0600

    2DO COMMIT PROYECTOS

--- P2/proyectos/calculosJS/operaciones.js
+++ P2/proyectos/calculosJS/operaciones.js
@@ -1,24 +1,33 @@
-function operacion() {
-    let n1 = parseFloat(document.getElementById("n1").value);
-    let n2 = parseFloat(document.getElementById("n2").value);
-    let tipoope = document.getElementById("tipo").value;
-    let ope = null;
 
-    // Validamos si los valores son números
-    if (isNumber(n1) && isNumber(n2)) {
-        switch (tipoope) {
-            case "+":ope = n1 + n2;break;
-            case "-":ope = n1 - n2;break;
-            case "*":ope = n1 * n2;break;
-            case "/":ope = n1 / n2;break;
-        }
-        let resul = document.getElementById("resultado");
-        resul.innerHTML = `<h2>${n1} ${tipoope} ${n2} = ${ope}</h2>`;
-    } else {
-        alert("Ingresa solo números");
-    }
+function operacion()
+{
+   let n1,n2,tipoope,ope,resul;
+
+   n1=parseFloat(document.getElementById("n1").value);
+   n2=parseFloat(document.getElementById("n2").value);
+   tipoope=document.getElementById("tipo").value;
+
+   if (isNumber(n1) && isNumber(n2))
+   {
+      switch (tipoope)
+      {
+         case "+":ope=n1+n2;break;
+         case "-":ope=n1-n2;break;
+         case "*":ope=n1*n2;break;
+         case "/":ope=n1/n2;break;
+      }
+   
+      resul=document.getElementById("resultado").innerHTML=`<h2>${n1} ${tipoope} ${n2} = ${ope}</h2>`
+   }
+   else
+   {
+      resul=document.getElementById("resultado").innerHTML=`<h2>Ingresa solo numeros ...</h2>`
+      alert('Ingresa solo numeros ...');
+   }
+
 }
 
-function isNumber(n) {
-    return !isNaN(n) && isFinite(n);
+function isNumber(n)
+{
+   return !isNaN(parseFloat(n) && isFinite(n))
 }
