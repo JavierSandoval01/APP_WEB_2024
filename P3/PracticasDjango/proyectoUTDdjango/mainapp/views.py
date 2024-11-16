2183c2bc9daa4ce159911b7fb0df2120667b9e54
Francisco Javier Vázquez Sandoval <javier.sandoval1123@gmail.com>
2024-11-15 18:41:56 -0600

    COMMIT P3 DJANGO V1

---
+++ P3/PracticasDjango/proyectoUTDdjango/mainapp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request,'mainapp/index.html',{
+        'title':'Inicio Pagina principal',
+        'content':'.:: Bienvenido a mi pagina principa ::.',
+    })
+    
+def about(request):
+    mensaje='Bienvenido, mi nombre es: Javier'
+    return render(request, 'mainapp/about.html',{
+        'title':'Acerca de nosostros',
+        'content':'Somos un grupo de desarrolladores de sw Multiplataforma',
+        'mensaje':'mensaje',
+    })
+    
+    
+
+    
+def mision(request):
+    return render(request,'mainapp/mision.html',{
+        'title':'vision',
+        'content':'.:: vienvenido a la mision de la empresa ::.'
+    })
+    
+
+def vision(request):
+    return render(request,'mainapp/vision.html',{
+        'title':'vision',
+        'content':'.:: vienvenido a la vision de la empresa ::.'
+    })
+    
+
+def redirigir_inicio(request,exception):
+    return render(request,'mainapp/404.html')
